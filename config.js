8c20bc1495bb3d34b15921de83f4769a15ea56ee
caheocon <95008077+caheocon@users.noreply.github.com>
2021-11-25 10:32:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    titleWeb: "Gửi thằng ah em ngu",
+    introTitle: 'chó bùi rẻ rách',
+    introDesc: `đụ má mày chứ nhìn cái loz địt mẹ hc dell lo loz què `,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    title: 'Phải chăng mày đã bị sì ke từ cái hôm m gặp tao 🥰',
+    desc: 'Phải chăng mày đã bị vô sinh vào ngày 1 tháng 1 2021 ',
+    btnYes: 'đúng rồi',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao mày lại bị ngu dữ z <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    reply: 'chơi thì chs ko chs thì cút  <33333333',
+    mess: 'tao biết mà 🥰.  ghét mày  nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 7h,nhà mày sáng nhất đêm nay .',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
